2cc2fe28b88eb826f5fc6ae130affb5173646725
Vladimir Scherbo <vscherbo@gmail.com>
2018-06-07 16:59:11 +0300

    added schema name

--- sql/cr-frontol_export_bill.sql
+++ sql/cr-frontol_export_bill.sql
@@ -1,4 +1,4 @@
-drop function cash.frontol_export_bill(integer);
+-- drop function cash.frontol_export_bill(integer);
 
 CREATE OR REPLACE FUNCTION cash.frontol_export_bill(arg_bill_no integer)
  RETURNS boolean
@@ -9,12 +9,12 @@ arg_order text[];
 res BOOLEAN;
 BEGIN
 SELECT array(SELECT txt FROM (
-SELECT 10 AS weight, frontol_order_header(arg_bill_no) AS txt
+SELECT 10 AS weight, cash.frontol_order_header(arg_bill_no) AS txt
 union
-SELECT 20 AS weight, frontol_order_items(arg_bill_no) AS txt
+SELECT 20 AS weight, cash.frontol_order_items(arg_bill_no) AS txt
 ORDER BY weight) AS f) INTO arg_order;
    -- RAISE NOTICE 'arg_order=%', arg_order;
-   res := sftp_text(arc_const('frontol_user'), arc_const('frontol_host'), 22, format('%s/order_%s.opn', arc_const('frontol_path'), arg_bill_no), arg_order);
+   res := cash.sftp_text(arc_const('frontol_user'), arc_const('frontol_host'), 22, format('%s/order_%s.opn', arc_const('frontol_path'), arg_bill_no), arg_order);
 
    RAISE NOTICE 'res=%', res;
    RETURN res;
