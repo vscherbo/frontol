b340df1bd66dfccf03516a1e6073c0ee3be7c034
root <root@vm-pg-devel.arc.world>
2018-07-03 11:52:36 +0300

    полный путь psql

--- ask-frontol.sh
+++ ask-frontol.sh
@@ -7,7 +7,7 @@ FLAG_FILE=$FLAG_DIR/frontol_receipts_flag.txt
 
 [ -f $FLAG_FILE ] && { logmsg INFO "Previous $FLAG_FILE exists. Exiting"; exit 0; }
 
-EXPECT_FLAG=`psql -qtAX -U arc_energo -c 'select exists (select * from cash.receipt_queue where status=0);'`
+EXPECT_FLAG=`/usr/bin/psql -qtAX -U arc_energo -c 'select exists (select * from cash.receipt_queue where status=0);'`
 logmsg INFO "EXPECT_FLAG=$EXPECT_FLAG"
 
 if [ +t == +$EXPECT_FLAG ]
