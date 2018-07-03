2e5a615190f7b687129f8d5cfb32651b540d3dfa
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-03 11:37:47 +0300

    добавлена запись в файл журнала

--- ask-frontol.sh
+++ ask-frontol.sh
@@ -2,8 +2,14 @@
 
 . /usr/local/bin/bashlib
 
-FLAG_DIR=/opt/atol/export_catalog
+BASE_DIR=/opt/atol
+FLAG_DIR=$BASE_DIR/export_catalog
 FLAG_FILE=$FLAG_DIR/frontol_receipts_flag.txt
+LOG_DIR=$BASE_DIR/logs
+LOG=$LOG_DIR/`basename $0`.log
+[ -d $LOG_DIR ] || mkdir -p $LOG_DIR
+
+exec 1>>$LOG 2>&1
 
 [ -f $FLAG_FILE ] && { logmsg INFO "Previous $FLAG_FILE exists. Exiting"; exit 0; }
 
