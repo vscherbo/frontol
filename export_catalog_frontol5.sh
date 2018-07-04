55e3c3eae8e2551ea00e7e90b36bdcae51559b56
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-04 09:58:07 +0300

    для запуска на сервере

--- export_catalog_frontol5.sh
+++ export_catalog_frontol5.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-#  не используем -h <hostname>, т.к. запускаем на PG
-psql -U arc_energo -f sql/copy_frontol_catalog2csv.sql |sed -r -e 's/["]{0,1};["]{0,1}/;/g' -e 's/""/"/g' |unix2dos| iconv -f utf8 -t cp1251//TRANSLIT -o /smb/it/ATOL/load-unload/frontol_catalog.txt
+DST=/opt/atol/export_catalog
+
+psql -U arc_energo -f copy_frontol_catalog2csv.sql |sed -r -e 's/["]{0,1};["]{0,1}/;/g' -e 's/""/"/g' |unix2dos| iconv -f utf8 -t cp1251//TRANSLIT -o $DST/frontol_catalog.txt 
+
+touch $DST/frontol_catalog_flag.txt
+
