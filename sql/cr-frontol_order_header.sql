f7d8236db391d90862d83504a8e7f03184c9fe6c
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-09 15:30:29 +0300

    в качестве суммы счёта используется сумма позиций "Сумма1"

--- sql/cr-frontol_order_header.sql
+++ sql/cr-frontol_order_header.sql
@@ -5,7 +5,7 @@ AS $function$
 SELECT concat_ws(';', 
 "№ счета", 1, to_char("Дата счета", 'DD.MM.YYYY'), --::date, 
 concat_ws(':', extract('hour' FROM dt_create), extract('minute' FROM dt_create))
-, "Сумма", '', "№ счета", '', '', '') || E'\r\n'
+, round("Счета"."Сумма1",2), '', "№ счета", '', '', '') || E'\r\n'
 FROM "Счета"
 WHERE "№ счета" = arg_bill_no;
 $function$
