e636036e6645c209242e69bd207e65a819e19ecb
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-09 15:40:55 +0300

    переменная SQL_SCRIPT

--- export_catalog_frontol5.sh
+++ export_catalog_frontol5.sh
@@ -2,7 +2,10 @@
 
 DST=/opt/atol/export_catalog
 
-psql -U arc_energo -f copy_frontol_catalog2csv.sql |sed -r -e 's/["]{0,1};["]{0,1}/;/g' -e 's/""/"/g' |unix2dos| iconv -f utf8 -t cp1251//TRANSLIT -o $DST/frontol_catalog.txt 
+# SQL_SCRIPT=copy_frontol_catalog2csv.sql
+SQL_SCRIPT=copy_frontol_catalog2csv-4update.sql
+
+psql -U arc_energo -f $SQL_SCRIPT |sed -r -e 's/["]{0,1};["]{0,1}/;/g' -e 's/""/"/g' |unix2dos| iconv -f utf8 -t cp1251//TRANSLIT -o $DST/frontol_catalog.txt 
 
 touch $DST/frontol_catalog_flag.txt
 
