daaa5a72dfe43b8122a7b7487668cf8917c95c98
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-09 15:42:09 +0300

    --amend

--- export_catalog_frontol5.sh
+++ export_catalog_frontol5.sh
@@ -3,7 +3,7 @@
 DST=/opt/atol/export_catalog
 
 # SQL_SCRIPT=copy_frontol_catalog2csv.sql
-SQL_SCRIPT=copy_frontol_catalog2csv-4update.sql
+SQL_SCRIPT=copy_frontol_catalog2csv_4update.sql
 
 psql -U arc_energo -f $SQL_SCRIPT |sed -r -e 's/["]{0,1};["]{0,1}/;/g' -e 's/""/"/g' |unix2dos| iconv -f utf8 -t cp1251//TRANSLIT -o $DST/frontol_catalog.txt 
 
