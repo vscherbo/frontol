6a838cbbc0cf6d4be4dec2bacc044a050b926d88
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-16 15:11:48 +0300

    процедура заполнения каталога товаров для копирования на кассу

---
+++ sql/cr-renew_ft_catalog.sql
@@ -0,0 +1,24 @@
+-- drop function cash.renew_ft_catalog();
+create function cash.renew_ft_catalog()
+returns void
+language sql
+as
+$body$
+truncate table cash.frontol_catalog;
+
+WITH arc2ft AS (SELECT c."КодСодержания" ks
+, substring(
+            format('%s %s', 
+                    c."КодСодержания", regexp_replace(regexp_replace("НазваниевСчет", '\n', ' ', 'g'), ';', '¤', 'g')
+                  ) 
+            from 1 for 128) ft_name
+, p."ОтпускнаяЦена"::numeric ft_price
+FROM "Содержание" c
+join "vwЦены" p on p."КодСодержания" = c."КодСодержания" 
+where "Активность"
+ORDER BY c."КодСодержания"
+)
+INSERT INTO cash.frontol_catalog(ft_code,ft_barcode,ft_name,ft_print_name,ft_price,ft_tax_group_code) (
+SELECT ks, ks, ft_name, ft_name, ft_price, 4 FROM arc2ft
+);
+$body$;
