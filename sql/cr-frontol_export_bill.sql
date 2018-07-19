903ed7e6cfc6cc39e5b572aa60d55d9217fe1b4b
Vladimir Scherbo <vscherbo@gmail.com>
2018-07-19 12:54:51 +0300

    возвращает код и текст для VBA MsgBox

--- sql/cr-frontol_export_bill.sql
+++ sql/cr-frontol_export_bill.sql
@@ -1,18 +1,26 @@
 -- drop function cash.frontol_export_bill(integer);
 
-CREATE OR REPLACE FUNCTION cash.frontol_export_bill(arg_bill_no integer, OUT out_res boolean, OUT out_rc integer)
+CREATE OR REPLACE FUNCTION cash.frontol_export_bill(arg_bill_no integer, OUT out_res boolean, OUT out_rc integer, OUT out_text text)
  RETURNS record
  LANGUAGE plpgsql
 AS $function$
 declare
 arg_order text[];
 loc_order_path text;
+str_canceled text := E'\r\nОперация отменена!';
+/* out_rc
+Успешно (Information): 64
+Exclamation:  48 
+Critical: 16
+*/
 BEGIN
 out_res := 'f'; -- initial 
-out_rc := 2; -- initial 
+out_rc := 16; -- initial. Critical
+out_text := E'ВНИМАНИЕ!\r\nОтправка на кассу не прошла, повторите операцию и/или обратитесь в отдел ИТ';
 PERFORM 1 FROM cash.receipt_queue WHERE bill_no = arg_bill_no;
 IF FOUND THEN
-    out_rc := 1; -- уже экспортирован
+    out_rc := 48; -- уже экспортирован, Excalmation
+    out_text := E'Этот счет уже был отправлен на кассу!' || str_canceled;
 ELSE
     SELECT array(SELECT txt FROM (
     SELECT 10 AS weight, cash.frontol_order_header(arg_bill_no) AS txt
@@ -25,15 +33,15 @@ ELSE
         else
             loc_order_path := 'devel_frontol_orders';
         end if;
-        -- res := cash.sftp_text(arc_const('frontol_user'), arc_const('frontol_host'), 22, format('%s/order_%s.opn', arc_const('frontol_path'), arg_bill_no), arg_order);
         out_res := cash.sftp_text(arc_const('frontol_user'), arc_const('frontol_host'), 22, format('%s/order_%s.opn', loc_order_path, arg_bill_no), arg_order);
         if out_res then
             insert into cash.receipt_queue(bill_no) values(arg_bill_no);
-            out_rc := 0;
+            out_rc := 64; -- Information
+            out_text := 'Отправка на кассу прошла успешно';
         end if;
 
 END IF;
-RAISE NOTICE 'res=%, rc=%', out_res, out_rc;
+RAISE NOTICE 'res=%, rc=%, text=%', out_res, out_rc, out_text;
 
 --    RETURN out_res;
 end;$function$
