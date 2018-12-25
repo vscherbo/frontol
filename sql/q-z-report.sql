73a2359f2ae5035722cdb240b3ddc9e2f451eaae
Vladimir Scherbo <vscherbo@gmail.com>
2018-12-25 16:57:35 +0300

    qualified ft_date

--- sql/q-z-report.sql
+++ sql/q-z-report.sql
@@ -8,5 +8,5 @@ FROM cash.vw_ft_report r
 left join (select c.ft_date, count(*) as rcpt_num from cash.vw_ft_close_doc c where c.ft_date = ft_date and c.op=0 group by ft_date) as sale on sale.ft_date= r.ft_date
 left join (select c.ft_date, count(*) as refund_num from cash.vw_ft_close_doc c where c.ft_date = ft_date and c.op=1 group by ft_date) as refund on refund.ft_date= r.ft_date
 WHERE r.ft_type = 63
-and ft_date = now()::date;
+and r.ft_date = now()::date;
 --and r.ft_date = '2018-07-13';                                                                                                                                                     
