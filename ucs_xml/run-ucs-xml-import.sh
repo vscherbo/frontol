f9ff38a742ec08a51238e11d1b5a0bdd208deda1
Vladimir Scherbo <vscherbo@gmail.com>
2023-04-05 14:46:02 +0300

    get yesterday report

--- ucs_xml/run-ucs-xml-import.sh
+++ ucs_xml/run-ucs-xml-import.sh
@@ -4,16 +4,17 @@
 . /usr/local/bin/bashlib
 
 WRK_DIR=$(dirname $0)
+
+pushd $WRK_DIR > /dev/null
 LOG=$(namename $0).log
-exec 1>>$LOG 2>&1
+exec 3>&1 1>>$LOG 2>&1
 
-pushd $WRK_DIR
-
-#DT_START=2022-11-28
-DT_START=$1
+YESTERDAY=$(date --date='yesterday' +%F)
+DT_START=${1:-$YESTERDAY}
+#DT_END=$(date --date="$DT_START + 1day" +%F)
 DT_END=$DT_START
-#DT_END=2022-11-28 
 
-/root/.pyenv/shims/python3 ./ucs_xml_report.py --log_level=INFO --dt_start=$DT_START # --dt_end=$DT_END
-/root/.pyenv/shims/python3 ./parse_ucs.py --xml=01-xml/report_txn_grp_txn_${DT_START}_${DT_END}.xml > rep.csv
+$HOME/.pyenv/shims/python ./ucs_xml_report.py --log_level=INFO --dt_start=$DT_START --dt_end=$DT_END
+$HOME/.pyenv/shims/python ./parse_ucs.py --xml=01-xml/report_txn_grp_txn_${DT_START}_${DT_END}.xml > rep.csv
 psql -h vm-pg.arc.world -U arc_energo -f rep.sql
+
