00a10723e465082f4364309825c3c72bb3fcbf97
Vladimir Scherbo <vscherbo@gmail.com>
2023-12-05 13:26:58 +0300

    Frontol 6

--- sql/cr-get_bill_by_cash_docnum.sql
+++ sql/cr-get_bill_by_cash_docnum.sql
@@ -2,5 +2,6 @@ CREATE OR REPLACE FUNCTION arc_energo.get_bill_by_cash_docnum(arg_doc_num intege
  RETURNS integer
  LANGUAGE sql
 AS $function$
-select order_id::integer as result from cash.vw_ft_close_doc where ft_doc_num = $1;
+-- Frontol5 select order_id::integer as result from cash.vw_ft_close_doc where ft_doc_num = $1;
+select order_id::integer as result from cash.vw_ft_close_doc_f6 where ft_doc_num = $1;
 $function$
